934a6d50c0c4de00fef0be7da8a2270700932da4
Diego Rocha <dkxever@gmail.com>
2022-03-15 20:45:36 -0300

    Styling our Header component

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,1 +1,38 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const Container = styled.section`
+  width: 100%;
+  padding: 30px 50px;
+  background-color: #ddd;
+`;
+
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+`;
+
+export const HeaderText = styled(Link)`
+  flex: 2;
+  margin-left: 3rem;
+  text-decoration: none;
+
+  h1{
+    font-size: 30px;
+    text-transform: uppercase;
+    color: black;
+  }
+`;
+
+export const FormHeader = styled.form`
+  flex: 1;
+  margin-right: 20px;
+`;
+
+export const NavButton = styled.nav`
+  flex: 1;
+  display: flex;
+  gap: 0.5rem;
+  width: 250px;
+`;
