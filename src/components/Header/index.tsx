47461bcd723abd085897180f170dc834103794aa
Diego Rocha <dkxever@gmail.com>
2022-03-15 20:43:47 -0300

    Structuring and organizing our Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,8 +4,36 @@ import * as C from "./styles";
 
 export const Header = () => {
   return(
-    <div>
-      ...
-    </div>
+    <C.Container>
+      <C.Header>
+        <C.HeaderText to="/" >
+          <h1>Crud Firebase</h1>
+        </C.HeaderText>
+        <C.FormHeader onSubmit={handleSearchForm}>
+          <Input
+            name="search"
+            type="text"
+            value={search}
+            onChange={({target}) => setSearch(target.value)}
+            placeholder="Procurar nome..."
+            autoComplete="off"
+          />
+        </C.FormHeader>
+        <C.NavButton>
+          <Button
+            path="/"
+            active={`${activeTab === 'home' ? "activetab":"home"}`}
+          >
+            Home
+          </Button>
+          <Button
+            path="/addcontact"
+            active={`${activeTab === 'addcontact' ? "activetab":"addcontact"}`}
+          >
+            Adicionar Contato
+          </Button>
+        </C.NavButton>
+      </C.Header>
+    </C.Container>
   )
 }
