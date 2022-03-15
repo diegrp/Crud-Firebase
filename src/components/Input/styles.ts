d075b5b588a81c96fac5045afd18acc6f5c090f0
Diego Rocha <dkxever@gmail.com>
2022-03-15 20:37:24 -0300

    Styling our Input component

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,1 +1,35 @@
 import styled from "styled-components";
+
+export const InputWrapper = styled.div`
+  input, select{
+    width: 100%;
+    padding: 17px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+  }
+`;
+
+export const SelectWrapper = styled.div`
+
+  margin-bottom: 20px;
+
+  select{
+    width: 100%;
+    display: inline-block;
+    background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAUCAMAAACtdX32AAAAdVBMVEUAAAD///8AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAhMdQaAAAAJ3RSTlMAAAECAwQGBwsOFBwkJTg5RUZ4eYCHkJefpaytrsXGy8zW3+Do8vNn0bsyAAAAYElEQVR42tXROwJDQAAA0Ymw1p9kiT+L5P5HVEi3qJn2lcPjtIuzUIJ/rhIGy762N3XaThqMN1ZPALsZPEzG1x8LrFL77DHBnEMxBewz0fJ6LyFHTPL7xhwzWYrJ9z22AqmQBV757MHfAAAAAElFTkSuQmCC);
+    background-position: 100%;
+    background-repeat: no-repeat;
+    border: 1px solid #ccc;
+    margin: 8px 0;
+    padding: 17px 20px;
+    border-radius: 4px;
+    appearance: none;
+    
+    &:required:invalid{
+      color: gray;
+    }
+  }
+
+`;
