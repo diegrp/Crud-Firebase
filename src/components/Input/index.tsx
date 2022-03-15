2594ced0ba2c4e2422d4a46a482a456d3ac6b629
Diego Rocha <dkxever@gmail.com>
2022-03-15 20:35:30 -0300

    Passing and arranging the props for our Input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,9 +1,37 @@
+import { ChangeEvent } from "react";
 import * as C from "./styles";
 
-// Input Component CRUD
+type Props = {
+  label?: string
+  id?: string
+  name: string
+  type: string
+  value: any
+  onChange: (e: ChangeEvent <HTMLInputElement>) => void
+  [x: string ]: any
+}
 
-export const Input = () => {
+type SelectProps = {
+  label?: string
+  id?: string
+  name: string
+  value?: any
+  onChange: ( e: ChangeEvent <HTMLInputElement | HTMLSelectElement> ) => void
+  [x: string ]: any
+}
+
+// Input e Select Component CRUD
+
+export const Input = ( { label, id, name, type, value, onChange, ...rest }: Props ) => {
   return(
+    <div>
+      ...
+    </div>
+  )
+}
+
+export const Select = ( { label, id, name, value, onChange, children, ...rest }: SelectProps ) => {
+  return (
     <div>
       ...
     </div>
