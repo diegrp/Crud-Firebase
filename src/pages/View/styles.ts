5d068146d68ebf0893b8e40bfd7aeee45e7abfa0
Diego Rocha <dkxever@gmail.com>
2022-03-15 21:30:36 -0300

    Styling our View page

--- src/pages/View/styles.ts
+++ src/pages/View/styles.ts
@@ -1,1 +1,39 @@
 import styled from "styled-components";
+
+export const Container = styled.section`
+  margin:auto;
+  max-width: 500px;
+`;
+
+export const Info = styled.div`
+  display: grid;
+  grid-template: 1fr 1fr;
+  gap: 1rem;
+`;
+
+export const Title = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #ddd;
+  padding: 20px;
+  border-radius: 10px;
+  
+  h1{
+    font-size: 28px;
+    margin-bottom: 10px;
+    color: #000;
+  }
+`;
+
+export const Description = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  padding: 20px;
+  border: 1px solid black;
+  border-radius: 10px;
+`;
+
