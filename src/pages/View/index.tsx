f56f3aeff0c62ca4c4475f7138c071ecd0c2c5c7
Diego Rocha <dkxever@gmail.com>
2022-03-15 21:29:06 -0300

    Structuring and organizing our View

--- src/pages/View/index.tsx
+++ src/pages/View/index.tsx
@@ -1,11 +1,44 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { User } from "../../types/User";
+import { viewDetailContact } from "../../services/firebase";
+import { Head } from "../../Router/Head";
 import * as C from "./styles";
 
 // Visualizar dados do usuário
 
 export const View = () => {
   return(
-    <div>
-      ...
-    </div>
+    <C.Container>
+      <Head title="Detalhes do Usuário" description="Dados cadastrais do contato na lista"/>
+      <C.Info>
+        <C.Title>
+            <h1>Detalhes do Usuário</h1>
+            <p>Lista de seus dados cadastrais</p>
+        </C.Title>
+        <C.Description>
+            <div>
+              <strong>Id: </strong>
+              <span>{id}</span>
+            </div>
+            <div>
+              <strong>Nome: </strong>
+              <span>{user.nome}</span>
+            </div>
+            <div>
+              <strong>E-mail: </strong>
+              <span>{user.email}</span>
+            </div>
+            <div>
+              <strong>Telefone: </strong>
+              <span>{user.telefone}</span>
+            </div>
+            <div>
+              <strong>Status: </strong>
+              <span style={{color: user.status !== "Ativo" ? "red":"green"}}>{user.status}</span>
+            </div>
+        </C.Description>
+      </C.Info>
+    </C.Container>
   )
 }
